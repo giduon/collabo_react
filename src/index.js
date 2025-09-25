f7d47c086bae11201b9f93db0567efcde544504a
giduon <giduon@naver.com>
2025-09-25 16:54:58 +0900

    2025-09-25

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css'; // for 부트 스트랩
 
-// Router는 App.js 파일 내의 모든 라우터 정보를 감싸는 역할을 합니다. 
-import { BrowserRouter } from 'react-router-dom'; // 신규생성됨
+// Router는 App.js 파일 내의 모든 라우터 정보를 감싸는 역할을 합니다.
+import { BrowserRouter } from 'react-router-dom'; // 신규 생성됨
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// StrickMode 코드삭제 : 개발 도중에 발생하는 문제를 추가적으로 감지 하기 위하여 rendering을 2번 수행합니다. 
+// StrictMode 코드 삭제 : 개발 도중에 발생하는 문제를 추가적으로 감지 하기 위하여 rendering을 2번 수행합니다.
 
 root.render(
   <BrowserRouter>
