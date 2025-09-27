7961f305af59d9f09c798a885bffb9bcd1f926c5
giduon <giduon@naver.com>
2025-09-27 17:46:52 +0900

    2025-09-27

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -18,6 +18,11 @@ function App({user}) {
 
     const navigate = useNavigate();
 
+    
+    // 장바구니 관련 코딩들
+    const [quantity, setQuantity] = useState(0);
+
+
     // 파라미터 id가 갱신이 되면 화면을 다시 rendering 시킵니다.
     useEffect(() => {
         const url = `${API_BASE_URL}/product/detail/${id}`;
@@ -57,6 +62,48 @@ function App({user}) {
         );
     }
 
+    // 수량 체인지 관련 이벤트 핸들러 함수 정의
+    const QuantityChange = (event) => {
+        // parseInt() 메소드는 정수형으로 생긴 문자열을 정수 값으로 변환해 줍니다. 
+        const newValue = parseInt(event.target.value);
+        setQuantity(newValue);
+    };
+
+    // 사용자가 수량을 입력하고, '장바구니' 버튼을 눌렀습니다.
+    const addToCart = async () => {
+        if(quantity <1) {
+            alert(`구매 수량은 1개 이상이어야 합니다.`);
+            return;
+        }
+        // alert(`${product.name} ${quantity} 개를 장바구니에 담기`);
+
+        try{
+            const url = `${API_BASE_URL}/cart/insert`;
+
+            // Cart에 담을 내용은 '회원 아이디', '상품 아이디', '수량' 입니다. 
+            // BackEnd 영억에서 CartProductDto 라는 클래스와 매치됩니다. 
+            const parameters = {
+                memberId: user.id,
+                productId: product.id,
+                quantity:quantity
+            }
+            
+            const response = await axios.post(url, parameters);
+
+            alert(response.data);
+            navigate('/product/list'); // 상품 목록 페이지로 이동 
+
+        }catch(error){
+            console.log(`오류 발생 : ` + error);
+
+            if(error.response){
+                alert('장바구니 추가 실패');
+                console.log(error.response.data);
+            }
+        }
+
+    }
+    
     return (
         <Container className = "my-4">
             <Card>
@@ -113,6 +160,8 @@ function App({user}) {
                                         type="number"
                                         min="1"
                                         disabled={!user}
+                                        value={quantity}
+                                        onChange={QuantityChange}
                                     />
                                 </Col>
                             </Form.Group>
@@ -122,7 +171,16 @@ function App({user}) {
                                 <Button variant="primary" className="me-3 px-4" href="/product/list">
                                     이전 목록
                                 </Button>
-                                <Button variant="success" className="me-3 px-4">
+                                <Button variant="success" className="me-3 px-4"
+                                    onClick={()=>{
+                                        if(!user){
+                                            alert('로그인이 필요한 서비스입니다.')
+                                            return navigate('/member/login');
+                                        }else{
+                                            addToCart();
+                                        }
+                                    }}
+                                >
                                     장바구니
                                 </Button>
                                 <Button variant="danger" className="me-3 px-4">
